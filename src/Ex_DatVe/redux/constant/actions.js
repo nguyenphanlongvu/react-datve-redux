160af1b7dc383398f0a88652b13dc01683fd9448
nguyenphanlongvu <vuvt6019@gmail.com>
2023-11-20 12:02:00 +0700

    update file

--- src/Ex_DatVe/redux/constant/actions.js
+++
@@ -1,1 +0,0 @@
-export const LUA_CHON = "LUA_CHON";
--- src/Ex_DatVe/redux/reducer/root.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { datVeReducer } from "./datVe";
-
-export let rootReducer = combineReducers({
-  datVeReducer: datVeReducer,
-});
